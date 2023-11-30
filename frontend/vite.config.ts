a1d17f8be30d0362ef74aba0e9dd076eaadf01be
sajeer <sajeersayed@gmail.com>
2023-11-30 15:18:43 +0300

    last5

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'window',
-    },
+  // define: {
+  //   global: 'window',
+  //   },
   server: {
     proxy: {
       // 'http://localhost:5173':'http://127.0.0.1:4444'
