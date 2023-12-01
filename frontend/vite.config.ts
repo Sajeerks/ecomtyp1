8d2b8be2d597f59d85c08dab60b6a105164cf376
sajeer <sajeersayed@gmail.com>
2023-12-01 07:37:43 +0300

    last10

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -20,15 +20,29 @@ export default defineConfig({
       //   secure:false,
       //   // ws:true
       // }
-
+   "/api/v1":'https://et1-4efd4e5170d2.herokuapp.com/'
   
-      "/api/v1": {
-        target: 'https://et1-4efd4e5170d2.herokuapp.com',
-        changeOrigin: true, 
+      // "/api/v1": {
+      //   target: 'https://et1-4efd4e5170d2.herokuapp.com/api/v1',
+      //   changeOrigin: true, 
         
-        secure:false,
-        // ws:true
-      }
+      //   secure:false,
+      //   // ws:true
+
+
+      //   // configure: (proxy, _options) => {
+      //   //   proxy.on('error', (err, _req, _res) => {
+      //   //     console.log('proxy error', err);
+      //   //   });
+      //   //   proxy.on('proxyReq', (proxyReq, req, _res) => {
+      //   //     console.log('Sending Request to the Target:', req.method, req.url);
+      //   //   });
+      //   //   proxy.on('proxyRes', (proxyRes, req, _res) => {
+      //   //     console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+      //   //   });
+      //   // }
+
+      // }
     }
 
   }
